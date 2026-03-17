598cc01ce833864ee2f31d6fa994ac81284a803c
Swan <swanand556@gmail.com>
2026-03-17 12:52:45 +0530

    added loading bar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header.js";
 import Main from "./Main.js";
+import Loader from "./Loader.js";
+import Error from "./Error.js";
+const initialState = {
+  questions: [],
+  status: "loading", // loading, error, ready, active, finished
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataRecieved":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    default:
+      throw new Error("Action Unknown");
+  }
+}
+
 export default function App() {
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  useEffect(function () {
+    fetch("http://localhost:9000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataRecieved", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
   return (
     <div className="app">
       <Header />
 
-      <Main>
-        <p>1/15</p>
-        <p>Question?</p>
-      </Main>
+      <Main>{status === "loading" && <Loader />}</Main>
     </div>
   );
 }
